c900b5bf1445a631dac4dae9cacef02f1ddff9a8
Kartik1745 <kartikpatil1997@gmail.com>
2023-04-10 17:01:09 -0400

    Lighthall Challenge

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [clickCount, setClickCount] = useState(0);
+  const [clickDistribution, setClickDistribution] = useState({});
+
+  const handleClick = () => {
+    setClickCount((count) => count + 1);
+  };
+
+  useEffect(() => {
+    const storedCount = parseInt(localStorage.getItem("clickCount"));
+    if (!isNaN(storedCount)) {
+      setClickCount(storedCount);
+    }
+    const storedDistribution = JSON.parse(localStorage.getItem("clickDistribution"));
+    if (storedDistribution) {
+      setClickDistribution(storedDistribution);
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("clickCount", clickCount.toString());
+    localStorage.setItem("clickDistribution", JSON.stringify(clickDistribution));
+  }, [clickCount, clickDistribution]);
+
+  const handleGeoClick = (e) => {
+    const { name } = e.target;
+    setClickDistribution((distribution) => ({
+      ...distribution,
+      [name]: (distribution[name] || 0) + 1,
+    }));
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Click Counter</h1>
+      <button onClick={handleClick}>Click me!</button>
+      <p>You have clicked the button {clickCount} times.</p>
+      <h2>Click Distribution by Geography</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>Country</th>
+            <th>Clicks</th>
+          </tr>
+        </thead>
+        <tbody>
+          {Object.entries(clickDistribution).map(([country, count]) => (
+            <tr key={country}>
+              <td>{country}</td>
+              <td onClick={handleGeoClick} name={country}>
+                {count}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
