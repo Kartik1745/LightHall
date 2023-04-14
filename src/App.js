b2bd45f4c24165d5bc226dd297db7a2a39bf6615
Kartik1745 <kartikpatil1997@gmail.com>
2023-04-14 11:17:47 -0400

    Done

--- src/App.js
+++ src/App.js
@@ -1,98 +1,37 @@
 import './App.css';
 import { useEffect, useState } from 'react';
-import axios from 'axios';
 
 function App() {
-  const [clickCount, setClickCount] = useState(0);
+  const [clickCount, setClickCount] = useState(parseInt(localStorage.getItem("clickCount")) || 0);
   const [clickDistribution, setClickDistribution] = useState({});
-  const [userCountry, setUserCountry] = useState("");
-
-  useEffect(() => {
-    axios
-      .get("https://ipapi.co/json/")
-      .then((response) => {
-        setUserCountry(response.data.country_name);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }, []);
-
-  useEffect(() => {
-    if (userCountry) {
-      axios
-        .get(`https://example.com/api/clicks?country=${userCountry}`)
-        .then((response) => {
-          setClickCount(response.data.count);
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-    }
-  }, [userCountry]);
+  
 
   const handleClick = () => {
     setClickCount((count) => count + 1);
-
-    axios
-      .post("https://example.com/api/clicks", {
-        country: userCountry,
-      })
-      .then((response) => {
-        setClickDistribution(response.data.distribution);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
   };
 
-  const handleGeoClick = (e) => {
-    const { name } = e.target;
+  useEffect(() => {
+    const storedCount = parseInt(localStorage.getItem("clickCount"));
+    if (!isNaN(storedCount)) {
+      setClickCount(storedCount);
+    }
+    const storedDistribution = JSON.parse(localStorage.getItem("clickDistribution"));
+    if (storedDistribution) {
+      setClickDistribution(storedDistribution);
+    }
+  }, []);
 
-    axios
-      .get(`https://example.com/api/clicks?country=${name}`)
-      .then((response) => {
-        setClickCount(response.data.count);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  };
+  useEffect(() => {
+    localStorage.setItem("clickCount", clickCount.toString());
+    localStorage.setItem("clickDistribution", JSON.stringify(clickDistribution));
+  }, [clickCount, clickDistribution]);
 
   return (
     <div className="App">
       <h1>Click Counter</h1>
       <button onClick={handleClick}>Click me!</button>
       <p>You have clicked the button {clickCount} times.</p>
-      <h2>Click Distribution by Geography</h2>
-      <table>
-        <thead>
-          <tr>
-            <th>Country</th>
-            <th>Clicks</th>
-          </tr>
-        </thead>
-        <tbody>
-          {Object.entries(clickDistribution).map(([country, count]) => (
-            <tr key={country}>
-              <td>{country}</td>
-              <td onClick={handleGeoClick} name={country}>
-                {count}
-              </td>
-            </tr>
-          ))}
-          {userCountry && (
-            <tr>
-              <td>{userCountry} (you)</td>
-              <td onClick={handleGeoClick} name={userCountry}>
-                {clickCount}
-              </td>
-            </tr>
-          )}
-        </tbody>
-      </table>
     </div>
   );
 }
-
 export default App;
